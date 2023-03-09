dcb1e9dd6ef1fca47b590ae0cf1324c3e7b552a4
Nik Alekseenko <nukalekseenko@gmail.com>
2023-03-09 14:00:28 +0300

    add Cat class and findCatByName method

--- src/Cat.java
+++ src/Cat.java
@@ -1,2 +1,50 @@
+import java.util.Objects;
+
 public class Cat {
+
+    private String name;
+
+    private String color;
+
+    public Cat(String name, String color) {
+        this.name = name;
+        this.color = color;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Cat cat = (Cat) o;
+        return Objects.equals(getName(), cat.getName()) && Objects.equals(getColor(), cat.getColor());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getName(), getColor());
+    }
+
+    @Override
+    public String toString() {
+        return "Cat{" +
+                "name='" + name + '\'' +
+                ", color='" + color + '\'' +
+                '}';
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,23 @@
 public class Main {
+    public static void main(String[] args) {
+        Cat cat1 = new Cat("Vasia", "Black");
+        Cat cat2 = new Cat("Sonia", "White");
+        Cat cat3 = new Cat("Monia", "Grey");
+
+        Cat[] cats = {cat1, cat2, cat3};
+
+        Cat resultCat = findCatByName("test", cats);
+
+        System.out.println(resultCat.getName());
+
+    }
+
+    public static Cat findCatByName(String name, Cat[] cats) {
+        for (Cat cat: cats) {
+            if (cat.getName().equals(name)) {
+                return cat;
+            }
+        }
+        return null;
+    }
 }
